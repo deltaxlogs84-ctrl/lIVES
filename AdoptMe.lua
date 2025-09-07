6979ea3f2887b0aef480fd195f0a82b386cf6009
deltaxlogs84-ctrl <deltaxlogs84@gmail.com>
2025-09-07 19:34:47 +0300

    Update AdoptMe.lua

--- AdoptMe.lua
+++ AdoptMe.lua
@@ -1,8 +1,19 @@
-local func
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local load = require(game.ReplicatedStorage:WaitForChild("Fsys")).load
 local TradeApp = require(game:GetService("ReplicatedStorage").ClientModules.Core.UIManager.Apps.TradeApp)
-local v_u_1 = require(game.ReplicatedStorage:WaitForChild("Fsys")).load
-local v_u_3 = v_u_1("RouterClient")
+local getData = require(game.ReplicatedStorage.ClientModules.Core.ClientData).get_data
+local petDatabase = require(game.ReplicatedStorage.ClientDB.Inventory.InventoryDB).pets
+local InventoryDB = require(game:GetService("ReplicatedStorage").ClientDB.Inventory.InventoryDB)
+
+local RouterClient = load("RouterClient")
+local ClientData = load("ClientData")
+local SendTradeRequest = RouterClient.get("TradeAPI/SendTradeRequest")
+
 local_offer = nil
+
+local func
 
 local old1
 local old2
@@ -68,19 +79,15 @@ local petstbl = {
 	"Pig",
 	"Pink Cat",
 	"Meerkat"
-
 }
-
 local function get_platform()
 	local userInputService = game:GetService("UserInputService")
-
 	if userInputService.TouchEnabled then
 		return "mobile"
 	else
 		return "pc"
 	end
 end
-
 for k,v in pairs(getgc(true)) do
 	if typeof(v) == "table" and rawget(v,"trade") then
 		for kk,vv in pairs(v['trade']) do
@@ -90,10 +97,8 @@ for k,v in pairs(getgc(true)) do
 		end
 	end
 end
-
 old1 = hookfunction(TradeApp._change_local_trade_state,function(a,b)
 	for k,v in pairs(b) do
-		print(k)
 		if k == "sender_offer" or k == "recipient_offer" then
 			for kk,vv in pairs(v) do
 				if kk == "items" then
@@ -117,7 +122,6 @@ old2 = hookfunction(TradeApp._overwrite_local_trade_state,function(a,b)
 	return old2(a,b)
 end)
 old3 = hookfunction(TradeApp._remove_item_from_my_offer,function(p174, p175)
-
 	local function findKey(alltables,needtable)
 		for k,v in pairs(alltables) do
 			if v["unique"] == needtable["unique"] then
@@ -125,7 +129,6 @@ old3 = hookfunction(TradeApp._remove_item_from_my_offer,function(p174, p175)
 			end
 		end
 	end
-
 	if p175.category == "houses" then
 		p174.UIManager.apps.HintApp:hint({
 			["text"] = "You can't remove this item.",
@@ -133,7 +136,7 @@ old3 = hookfunction(TradeApp._remove_item_from_my_offer,function(p174, p175)
 			["overridable"] = true
 		})
 	else
-		v_u_3.get("TradeAPI/RemoveItemFromOffer"):FireServer(p175.unique)
+		RouterClient.get("TradeAPI/RemoveItemFromOffer"):FireServer(p175.unique)
 		local v176, v177 = p174:_get_my_offer()
 		local v178 = findKey(v176.items,p175)
 		table.remove(v176.items, v178)
@@ -149,13 +152,10 @@ old3 = hookfunction(TradeApp._remove_item_from_my_offer,function(p174, p175)
 end)
 old4 = hookfunction(func,function(...)
 	local args = {...}
-
 	if #args > 2 then
 		for k,v in pairs(args) do
 			if typeof(v) == "table" then
-				print(local_offer)
 				if local_offer ~= nil then
-
 					if game.Players.LocalPlayer == v.sender then
 						v["sender_offer"]["items"] = local_offer
 					else
@@ -165,43 +165,33 @@ old4 = hookfunction(func,function(...)
 			end
 		end
 	else
-
 	end
-
 	return old4(unpack(args))
 end)
 old5 = hookfunction(TradeApp.hide,function(...)
-
-	local v_u_1 = require(game.ReplicatedStorage:WaitForChild("Fsys")).load
-	local v_u_10 = v_u_1("ClientData")
-
 	if local_offer ~= nil then
-		for k,v in pairs(v_u_10.get("inventory")["pets"]) do
+		for k,v in pairs(ClientData.get("inventory")["pets"]) do
 			for kk,vv in pairs(local_offer) do
 				if vv['unique'] == k then
-					v_u_10.get("inventory")["pets"][k] = nil
+					ClientData.get("inventory")["pets"][k] = nil
 				end
 			end
 		end
 	end
-
 	local_offer = nil
 	return old5(...)
 end)
-
 getgenv().ForcedPartnerName = nil
 local SavedApp = nil
 local function UpdateNamesTradeApp()
 	if not SavedApp then return end
-
+	local UDim2New = get_platform() == "mobile" and 140 or 220
 	if getgenv().ForcedPartnerName then
 		pcall(function()
-			local v200 = get_platform() == "mobile" and 140 or 220
-
 			if SavedApp.negotiation_partner_name_label then
 				SavedApp.negotiation_partner_name_label.Text = getgenv().ForcedPartnerName
-				SavedApp.negotiation_partner_name_label.Size = UDim2.new(0, v200, 1, 0)
-				SavedApp.negotiation_partner_name_label.Size = UDim2.new(0, math.min(v200, SavedApp.negotiation_partner_name_label.TextBounds.X), 1, 0)
+				SavedApp.negotiation_partner_name_label.Size = UDim2.new(0, UDim2New, 1, 0)
+				SavedApp.negotiation_partner_name_label.Size = UDim2.new(0, math.min(UDim2New, SavedApp.negotiation_partner_name_label.TextBounds.X), 1, 0)
 			end
 			if SavedApp.confirmation_partner_name_label then
 				SavedApp.confirmation_partner_name_label.Text = getgenv().ForcedPartnerName
@@ -212,8 +202,8 @@ local function UpdateNamesTradeApp()
 			if SavedApp.state then
 				if SavedApp.negotiation_partner_name_label and SavedApp.state._original_negotiation_name then
 					SavedApp.negotiation_partner_name_label.Text = SavedApp.state._original_negotiation_name
-					SavedApp.negotiation_partner_name_label.Size = UDim2.new(0, v200, 1, 0)
-					SavedApp.negotiation_partner_name_label.Size = UDim2.new(0, math.min(v200, SavedApp.negotiation_partner_name_label.TextBounds.X), 1, 0)	
+					SavedApp.negotiation_partner_name_label.Size = UDim2.new(0, UDim2New, 1, 0)
+					SavedApp.negotiation_partner_name_label.Size = UDim2.new(0, math.min(UDim2New, SavedApp.negotiation_partner_name_label.TextBounds.X), 1, 0)	
 				end
 				if SavedApp.confirmation_partner_name_label and SavedApp.state._original_confirmation_name then
 					SavedApp.confirmation_partner_name_label.Text = SavedApp.state._original_confirmation_name
@@ -222,11 +212,9 @@ local function UpdateNamesTradeApp()
 		end)
 	end
 end
-
 local old_refresh_all
 old_refresh_all = hookfunction(TradeApp.refresh_all, function(app, ...)
 	SavedApp = app
-
 	if app.state then
 		if app.negotiation_partner_name_label then
 			app.state._original_negotiation_name = app.negotiation_partner_name_label.Text
@@ -235,12 +223,10 @@ old_refresh_all = hookfunction(TradeApp.refresh_all, function(app, ...)
 			app.state._original_confirmation_name = app.confirmation_partner_name_label.Text
 		end
 	end
-
 	local result = old_refresh_all(app, ...)
 	UpdateNamesTradeApp()
 	return result
 end)
-
 getgenv().UsedAges = getgenv().UsedAges or {}
 local function getUniqueRandomAge()
 	local age
@@ -250,13 +236,11 @@ local function getUniqueRandomAge()
 	getgenv().UsedAges[age] = true
 	return age
 end
-
 local function makePetPattern(id, kind, unique, neon, mega, fly, ride, stacks)
 	local ageRandom = 3
 	if stacks then
 		ageRandom = getUniqueRandomAge()
 	end
-
 	if neon then
 		neon = true
 		mega = false
@@ -264,7 +248,6 @@ local function makePetPattern(id, kind, unique, neon, mega, fly, ride, stacks)
 		neon = false
 		mega = true
 	end
-
 	return {
 		["unique"] = unique,
 		["category"] = "pets",
@@ -284,7 +267,6 @@ local function makePetPattern(id, kind, unique, neon, mega, fly, ride, stacks)
 		["newness_order"] = 0
 	}
 end
-
 local usedUniques = {}
 local function createUnique()
 	local chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -296,41 +278,26 @@ local function createUnique()
 		end
 	until not usedUniques[Unique]
 	usedUniques[Unique] = true
-	return "2_" .. Unique
+	return "2_" .. Unique .. "_iPad"
 end
-
 local function createPet(name,fly,ride,neon,meganeon, stacks)
-	local m = require(game:GetService("ReplicatedStorage").ClientDB.Inventory.InventoryDB)
-	for k,v in pairs(m.pets) do
+	for k,v in pairs(InventoryDB.pets) do
 		if v["name"]:lower() == name:lower() then
 			local uniq = "2_"..createUnique()
-			local v_u_1 = require(game.ReplicatedStorage:WaitForChild("Fsys")).load
-			local v_u_10 = v_u_1("ClientData")
-			v_u_10.get("inventory")["pets"][uniq] = makePetPattern(v['id'],v['kind'],uniq,neon,meganeon,fly,ride, stacks)
+			ClientData.get("inventory")["pets"][uniq] = makePetPattern(v['id'],v['kind'],uniq,neon,meganeon,fly,ride, stacks)
 		end
 	end
-end
-local function isPetExist(petname)
-	local m = require(game:GetService("ReplicatedStorage").ClientDB.Inventory.InventoryDB)
-	for k,v in pairs(m.pets) do
-		if v["name"] == petname then
-			return true
-		end
-	end
-	return false
 end
 
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
+
 
 local ImGui =  loadstring(game:HttpGet('https://pastebin.com/raw/3Sp2zUFP'))()
 local Window = ImGui:CreateWindow({
-	Title = "Adopt Me | Live Helper",
+	Title = "Adopt Me | ",
 	Size = UDim2.new(0, 200, 0, 250),
 	Position = UDim2.new(0.10, 0, 0, 70)
 })
 Window:Center()
-
 local AdoptMeHelper = {
 	Nicknames = {
 		Nickname = nil,
@@ -343,26 +310,12 @@ local AdoptMeHelper = {
 		SelectedBlockPlayer = nil
 	}
 }
-
-local load = require(game.ReplicatedStorage:WaitForChild("Fsys")).load
-
-local getData = require(game.ReplicatedStorage.ClientModules.Core.ClientData).get_data
-local petDatabase = require(game.ReplicatedStorage.ClientDB.Inventory.InventoryDB).pets
-local RouterClient = load("RouterClient")
-local SendTradeRequest = RouterClient.get("TradeAPI/SendTradeRequest")
-
-local negotiationFrameHeader = LocalPlayer.PlayerGui.TradeApp.Frame.NegotiationFrame.Header
-local partFrame = negotiationFrameHeader.PartnerFrame
-local confrirmHeader = LocalPlayer.PlayerGui.TradeApp.Frame.ConfirmationFrame
-local part2Frame = LocalPlayer.PlayerGui.TradeApp.Frame.ConfirmationFrame.PartnerLabel
-local cloned = partFrame:Clone()
-
 local TradeTab = Window:CreateTab({
 	Name = "Trade",
 	Visible = true
 })
 TradeTab:Separator({Text = "Change Nicknames"})
-local StatusNicknameText 
+local StatusNicknameText
 local StatusNicknameStatusText
 local PlayerCombo = TradeTab:Combo({
 	Placeholder = "Select player",
@@ -426,7 +379,6 @@ TradeTab:Button({
 			end
 			return nil
 		end
-
 		local randomFriend = getRandomFriend()
 		if not randomFriend then return end
 		getgenv().ForcedPartnerName = randomFriend.Username
@@ -437,7 +389,6 @@ TradeTab:Button({
 		UpdateNamesTradeApp()
 	end
 })
-
 TradeTab:Button({
 	Text = "Reload",
 	Callback = function(self)
@@ -447,20 +398,14 @@ TradeTab:Button({
 		StatusNicknameText.Text = "Nickname: none"
 		StatusNicknameStatusText.Text = "Status: Not Changed"
 		PlayerCombo:SetPlaceholder("Select player")
-
 		UpdateNamesTradeApp()
 	end
 })
-
-
 TradeTab:Separator({Text = "Status"})
-
 StatusNicknameText = TradeTab:Label()
 StatusNicknameStatusText = TradeTab:Label()
-
 StatusNicknameText.Text = "Nickname: none"
 StatusNicknameStatusText.Text = "Status: Not Changed"
-
 local function UpdatePlayers()
 	local items = {}
 	for _, plr in ipairs(Players:GetPlayers()) do
@@ -472,7 +417,6 @@ end
 Players.PlayerAdded:Connect(UpdatePlayers)
 Players.PlayerRemoving:Connect(UpdatePlayers)
 UpdatePlayers()
-
 TradeTab:Separator({Text = "Actions"})
 local TradePlayerCombo = TradeTab:Combo({
 	Placeholder = "Select player",
@@ -521,6 +465,9 @@ end
 Players.PlayerAdded:Connect(UpdateTradePlayers)
 Players.PlayerRemoving:Connect(UpdateTradePlayers)
 UpdateTradePlayers()
+
+
+
 local PetsTab = Window:CreateTab({
 	Name = "Inv",
 	Visible = false
@@ -568,6 +515,7 @@ PetsTab:Button({
 })
 
 
+
 local MiscTab = Window:CreateTab({
 	Name = "Misc",
 	Visible = false
@@ -593,18 +541,12 @@ MiscTab:InputText({
 		BlockPlayerCombo.Items = filtered
 	end
 })
-local btn
 MiscTab:Button({
 	Text = "Block player",
 	Callback = function()
 		local Selected = AdoptMeHelper.Misc.SelectedBlockPlayer
 		if Selected then
 			game:GetService("StarterGui"):SetCore("PromptBlockPlayer", Selected)
-			--[[
-			wait(0.1)
-			btn = game.CoreGui.BlockingModalScreen.BlockingModalContainer.BlockingModalContainerWrapper.BlockingModal.AlertModal.AlertContents.Footer.Buttons["3"]
-			firesignal(btn.MouseButton1Click, btn)
-			]]
 		end
 	end
 })
@@ -620,6 +562,8 @@ Players.PlayerAdded:Connect(UpdateTradePlayers)
 Players.PlayerRemoving:Connect(UpdateTradePlayers)
 UpdateTradePlayers()
 
+
+
 spawn(function()
 	local LastPosition = nil
 	game:GetService("RunService").Heartbeat:Connect(function()
@@ -629,7 +573,6 @@ spawn(function()
 				PrimaryPart.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
 				PrimaryPart.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
 				PrimaryPart.CFrame = LastPosition
-
 				game.StarterGui:SetCore("ChatMakeSystemMessage", {
 					Text = "You were flung. Neutralizing velocity.";
 					Color = Color3.fromRGB(255, 0, 0);
